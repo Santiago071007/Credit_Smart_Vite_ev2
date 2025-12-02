b6a61622a582eefb49a13e3075722a52609d65b1
Santiago Torres <santiagocantor23@gmail.com>
2025-12-02 18:38:22 -0500

    Cálculo de cuota mensual funcionando

--- src/pages/Pedir.jsx
+++ src/pages/Pedir.jsx
@@ -1,4 +1,4 @@
-// Validaciones: email, cédula y monto mínimo implementadas
+// Función calcularCuota probada y funcionando
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Pedir.css";
